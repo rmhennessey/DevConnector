f6d384808c6d19bffa3eae8df2868bc75febf6f3
RMH <rory.hennessey@gmail.com>
2019-12-07 11:06:54 -0500

    Add profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -157,6 +157,63 @@ router.delete('/', auth, async (req, res) => {
     console.error(err.message);
     res.status(500).send('Server Error')
   }
-})
+});
+
+// @route   PUT api/profile/experience
+// @desc    Add profile experience
+// @access  Private
+router.put('/experience', [auth,
+  [
+    check('title', 'Title is required')
+      .not()
+      .isEmpty(),
+    check('company', 'Company is required')
+      .not()
+      .isEmpty(),
+    check('from', 'From date is required')
+      .not()
+      .isEmpty(),
+  ]
+],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    }
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id }) // we get this id from the token...
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (err) {
+      console.err(err.message);
+      res.status(500).send('Server Error')
+
+    }
+  })
 
 module.exports = router;
