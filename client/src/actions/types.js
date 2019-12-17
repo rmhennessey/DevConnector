9273c0ad9b8da0fcc57cfa2488196ff70053315c
RMH <rory.hennessey@gmail.com>
2019-12-17 16:47:53 -0500

    Alert Component & Action Call

---
+++ client/src/actions/types.js
@@ -0,0 +1,5 @@
+// This gives us a centralized filed to see all action types in one spot
+// Changes made here will apply throughout app, versus having to change in each spot
+
+export const SET_ALERT = "SET_ALERT";
+export const REMOVE_ALERT = "REMOVE_ALERT"
