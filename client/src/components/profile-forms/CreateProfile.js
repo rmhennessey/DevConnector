6d86f919dfacb7c03077523a089770aaeb589364
RMH <rory.hennessey@gmail.com>
2019-12-18 18:08:37 -0500

    Create Profile Action

--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,8 +1,10 @@
 import React, { Fragment, useState } from 'react'
+import { Link, withRouter } from 'react-router-dom'// we need withRouter in order to use history.push
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import { createProfile } from '../../actions/profile'
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -37,6 +39,11 @@ const CreateProfile = props => {
 
   const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
+
   return (
     <div>
       <h1 className="large text-primary">
@@ -47,7 +54,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select name="status" value={status} onChange={e => onChange(e)}>
             <option value="0">* Select Professional Status</option>
@@ -141,6 +148,9 @@ const CreateProfile = props => {
   );
 };
 
-CreateProfile.propTypes = {}
+CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired,
+}
 
-export default CreateProfile;
+
+export default connect(null, { createProfile })(withRouter(CreateProfile)); //wrapping in withRouter() allows us to access history.push
