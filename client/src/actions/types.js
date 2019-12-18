e41e88bf9aee99070200efd5fb1b5ebe9a3fc54e
RMH <rory.hennessey@gmail.com>
2019-12-18 08:03:15 -0500

    Auth Reducer & Registration

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -2,4 +2,6 @@
 // Changes made here will apply throughout app, versus having to change in each spot
 
 export const SET_ALERT = "SET_ALERT";
-export const REMOVE_ALERT = "REMOVE_ALERT"
+export const REMOVE_ALERT = "REMOVE_ALERT";
+export const REGISTER_SUCCESS = "REGISTER_SUCCESS";
+export const REGISTER_FAIL = "REGISTER_FAIL";
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -2,10 +2,11 @@ import React, { Fragment, useState } from 'react';
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { setAlert } from '../../actions/alert';
+import { register } from '../../actions/auth';
 import PropTypes from 'prop-types';
 
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -24,7 +25,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger')
     } else {
-      console.log('SUCCESS')
+      register({ name, email, password }); // pulled out from form data
     }
   }
   return (
@@ -39,11 +40,11 @@ const Register = ({ setAlert }) => {
             name="name"
             value={name}
             onChange={e => onChange(e)}
-            required />
+          />
         </div>
         <div className="form-group">
           <input type="email" placeholder="Email Address" name="email" value={email}
-            onChange={e => onChange(e)} required />
+            onChange={e => onChange(e)} />
           <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email</small>
@@ -55,7 +56,6 @@ const Register = ({ setAlert }) => {
             name="password"
             value={password}
             onChange={e => onChange(e)}
-            minLength="6"
           />
         </div>
         <div className="form-group">
@@ -65,7 +65,6 @@ const Register = ({ setAlert }) => {
             name="password2"
             value={password2}
             onChange={e => onChange(e)}
-            minLength="6"
           />
         </div>
         <input type="submit" className="btn btn-primary" value="Register" />
@@ -78,7 +77,8 @@ const Register = ({ setAlert }) => {
 }
 
 Register.propTypes = {
-  setAlert: PropTypes.func.isRequired
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired
 }
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
